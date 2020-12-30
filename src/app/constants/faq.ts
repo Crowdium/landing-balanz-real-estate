ecd2261a7421ca8fc18560f1406c9143d7b8624a
Nazareno Ciancaglini <nazareno.ciancaglini@gmail.com>
2020-12-30 12:58:02 -0300

    Fix: agrego preguntas frecuentes

--- src/app/constants/faq.ts
+++ src/app/constants/faq.ts
@@ -19,101 +19,132 @@ export const faq: ICollapseMenuData[] = [
         `
     },
     {
-        title: "¿Qué garantías tienen estos proyectos en general?",
+        title: "¿Hay algún límite de capital para participar?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                El monto máximo que podrás ingresar será determinado por la fiduciaria CFA, dependiendo de la declaración de ingresos que adjuntaste.
             </p>
         `
     },
     {
-        title: "¿Como recibo el pago de los intereses y el recupero de mi inversión?",
+        title: "¿Es posible participar más capital luego de haber realizado la participación inicial?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                Siempre que el proyecto inmobiliario se encuentre disponible para participar, los inversores podrán aumentar el capital inicial invertido en ese proyecto. 
+                Una vez finalizado el tiempo disponible para participar en un proyecto, los inversores NO tendrán que realizar aportes adicionales en ningún momento.
             </p>
         `
     },
     {
-        title: "¿Monto mínimo de inversión?",
+        title: "¿Qué ocurre si no se recauda el 100% del capital requerido para un proyecto en el tiempo preestablecido?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                Si el proyecto no reúne el capital requerido, el dinero será devuelto a los usuarios en su totalidad, sin descontar ninguna comisión.
             </p>
         `
     },
     {
-        title: "¿Qué gastos de comisiones tengo por invertir proyectos de Real Estate?",
+        title: "¿Cuándo voy a recuperar mi participación y cómo van a ser distribuidas las potenciales ganancias?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                La participación y la potencial ganancia se recuperan con la venta de los activos del proyecto y/o con eventuales rentas, dependiendo del proyecto.
             </p>
         `
     },
     {
-        title: "¿Hay una inversión mínima?",
+        title: "¿Qué documentación voy a recibir al participar en Crowdium?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                El fiduciante recibirá una nota de recibo de aportes en donde ya se lo considera fiduciante. Esta nota estará firmada por el fiduciario.
             </p>
         `
     },
     {
-        title: "¿Cómo transfiero el dinero para invertir en fondos de Balanz?",
+        title: "¿Existe un Mercado Secundario donde pueda vender mi participación?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                No. Crowdium no ofrece ni promociona ningún tipo de sistema de mercado secundario por el cual un fiduciante pueda ofrecer libremente su participación a otros. 
+                Inclusive, la empresa desalienta la salida de fiduciantes antes de la finalización del contrato de fideicomiso agregando una cláusula a dicho contrato una 
+                penalidad por rescisión anticipada del contrato de fideicomiso. En caso que un fiduciante solicite la rescisión anticipada del contrato de fideicomiso, y 
+                que un nuevo fiduciante ya registrado en la plataforma con cuenta activa (es decir, ya habiendo pasado por la aprobación de documentación de la fiduciaria) 
+                desee ingresar al fideicomiso como nuevo fiduciante, la fiduciaria podrá aceptar la solicitud de rescisión anticipada, y cancelar la participación en el 
+                fideicomiso. No existe la transferencia de titularidad entre fiduciantes, ni total, ni parcial. Todos los involucrados y procesos antes detallados quedan 
+                registrados y aprobados por la fiduciaria.
             </p>
         `
     },
     {
-        title: "¿Cómo retiro mi capital?",
+        title: "¿Cuál es el riesgo de participar?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                El riesgo de participar en Crowdium es equivalente a cualquier compra que se realice en el mercado de bienes inmuebles.
             </p>
         `
     },
     {
-        title: "¿Cuáles son los tiempos de los proyectos?",
+        title: "¿Qué sucede si las participaciones en una determinada cartera superan el monto necesario para el proyecto?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                Una vez recaudado el 100% del capital requerido para el proyecto se cerrarán las participaciones y solos quienes hicieron efectiva su participación antes de 
+                ese momento participarán de ese determinado proyecto.
+            </p>
+            <p>
+                El resto de los potenciales usuarios podrán optar por cualquier otro proyecto de su interés que este activo en ese momento.
             </p>
         `
     },
     {
-        title: "¿Cómo voy a poder informarme sobre los avances de mis participaciones?",
+        title: "¿Qué ocurre si no se reune el capital requerido para un proyecto en el tiempo preestablecido?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                Si el proyecto no se fondea en los niveles requeridos, el dinero será devuelto a los usuarios en su totalidad, sin descontar ninguna comisión.
             </p>
         `
     },
     {
-        title: "¿Qué diferencia hay entre participar en esta inversión o invertir en un fideicomiso inmobiliario al costo?",
+        title: "¿Cómo hago para transferir los fondos al Fideicomiso?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                respuesta de BALANZ
             </p>
         `
     },
     {
-        title: "¿Cuál es el riesgo de participar en estos proyectos?",
+        title: "¿Quién administra el capital?",
         content: `
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores illo vitae cumque impedit minima asperiores aliquam ipsam unde obcaecati, 
-                inventore aut, minus exercitationem, quia eos quo alias dolorem? Corrupti.
+                Al ingresar en Crowdium, tu participación será depositada en un reconocido banco y administrada a través de CFA, una fiduciaria financiera independiente de Crowdium.
+            </p>
+        `
+    },
+    {
+        title: "¿Qué es un fideicomiso?",
+        content: `
+            <p>
+                Un fideicomiso es un contrato regulado en la Ley 24.441 y, en términos generales, es una figura jurídica a través del cual una persona (fiduciante) se obliga a 
+                transmitir a otra (fiduciario) una determinada cantidad de bienes para que este los administre durante un cierto tiempo o hasta el cumplimiento de una determinada 
+                condición, con el encargo de transmitirlos al cumplimiento de ese tiempo o de esa condición a una tercera persona llamada beneficiario.
+            </p>
+            <p>
+                En otras palabras un fideicomiso es un contrato entre partes. En el caso de Crowdium el Fiduciante o inversor, que es quien transfiere los bienes (el dinero), 
+                es también el Beneficiario y el único rol del fiduciario es el de administrar los bienes de acuerdo a los fines específicos del contrato.
+            </p>
+            <p>
+                La principal razón por la que se elige la constitución de un fideicomiso es la seguridad jurídica, donde se contruye un patrimonio común separado de los 
+                bienes individuales de sus integrantes (inversores), del intermediario (Crowdium) y de la propiedad inmediata en la que se invierte.
+            </p>
+            <p>
+                De esta manera se consigue transparencia y seguridad, que permite el acceso a rendimientos de proyectos para los cuales se necesitaría mucho capital si se 
+                deseara participar individualmente.
+            </p>
+        `
+    },
+    {
+        title: "¿Quién es la fiduciaria CFA S.A?",
+        content: `
+            <p>
+                CFA Compañía Fiduciaria Americana S.A., es una reconocida fiduciaria de primer nivel y registrada ante la CNV (Comisión Nacional de Valores), 
+                que administra todos los fondos que se transfieren en una cuenta bancaria de un reconocido banco.
             </p>
         `
     },
