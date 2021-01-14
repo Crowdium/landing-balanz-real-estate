0b15bfbe9095a14000fefd55be542e97e4d5a5c7
Nazareno Ciancaglini <nazareno.ciancaglini@gmail.com>
2021-01-14 17:37:05 -0300

    Fix: Cambios textos

--- src/app/constants/timeline.ts
+++ src/app/constants/timeline.ts
@@ -1,7 +1,7 @@
 import { ITimeline } from '../components/timeline/timeline.component';
 
 export const timelineData: ITimeline[] = [
-    {step: `<icon-doughnut></icon-doughnut>`, title: "Participación:", content: "Adquirís una cuota parte de un fideicomiso ordinario privado que es dueño del activo inmobiliario."},
+    {step: `<icon-doughnut></icon-doughnut>`, title: "Participación:", content: "Adquirís un porcentaje de participación en un fideicomiso ordinario privado que es dueño del activo inmobiliario."},
     {step: `<icon-bars></icon-bars>`, title: "Renta y Revalorización:", content: "Tu capital estará invertido en una propiedad que se revalorizará con el tiempo y dependiendo del tipo de proyecto, se cobrarán los retornos."},
     {step: `<icon-money></icon-money>`, title: "Retornos:", content: "Al finalizar el período estimado del proyecto, los activos inmobiliarios se colocarán a la venta y se distribuirán los retornos generados por éstas."},
 ]
