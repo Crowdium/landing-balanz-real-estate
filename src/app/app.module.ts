d859caddff50142834cb94642adb336c0b92860b
Nazareno Ciancaglini <nazareno.ciancaglini@gmail.com>
2021-02-11 12:09:18 -0300

    Fix: Remuevo Bootstrap Libreria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './screens/app-routing.module';
 import { AppComponent } from './app.component';
 import { ScreensModule } from './screens/screens.module';
@@ -9,7 +8,6 @@ import { ComponentsModule } from './components/components.module';
 import { ContainersModule } from './containers/containers.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -25,7 +23,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     FormsModule,
     ReactiveFormsModule,
     NzIconModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent],
